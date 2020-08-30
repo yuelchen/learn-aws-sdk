d69154559cbe386cb43c6ef3f6ab11d5f2d19721
yuelchen <ychen.workspace@gmail.com>
2020-08-29 23:40:53 -0400

    Updated JavaDocs.

--- IntroGlueAPI/src/main/java/com/yuelchen/main/UsingGlueApiClient.java
+++ IntroGlueAPI/src/main/java/com/yuelchen/main/UsingGlueApiClient.java
@@ -1,13 +1,23 @@
 package com.yuelchen.main;
 
 import java.util.List;
-
 import com.amazonaws.services.glue.model.Database;
 import com.amazonaws.services.glue.model.Table;
 import com.example.glue.GlueApiClient;
 
+/**
+ * 
+ * @author 	Yue Chen
+ * @version	1.0.0
+ * @since 	2019-09-01
+ */
 public class UsingGlueApiClient {
 	
+	/**
+	 * The main method. 
+	 * 
+	 * @param args			an array of Strings as main method arguments.
+	 */
 	public static void main(String[] args) {
 		
 		//create use-case data
