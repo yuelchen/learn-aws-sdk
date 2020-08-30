054c0333c6f8d36fe0f60368cd7b0573b291d4ac
yuelchen <ychen.workspace@gmail.com>
2020-08-30 18:30:36 -0400

    Initial commit.

---
+++ IntroS3Api/src/main/java/com/yuelchen/main/UsingS3ApiClient.java
@@ -0,0 +1,20 @@
+package com.yuelchen.main;
+
+/**
+ * 
+ * @author 	Yue Chen
+ * @version	1.0.0
+ * @since 	2019-09-01
+ */
+public class UsingS3ApiClient {
+	
+	/**
+	 * The main method. 
+	 * 
+	 * @param args			an array of Strings as main method arguments.
+	 */
+	public static void main(String[] args) {
+		
+		
+	}
+}
